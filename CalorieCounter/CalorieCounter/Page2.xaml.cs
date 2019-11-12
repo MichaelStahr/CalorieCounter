8c8e60ce205be7f49728fc1ac7ac63b98224c759
Alec Bird <abird1231@gmail.com>
2019-11-12 16:24:09 -0500

    Added ability to display data directly from db (Sam's update)

--- CalorieCounter/CalorieCounter/Page2.xaml.cs
+++ CalorieCounter/CalorieCounter/Page2.xaml.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using System.Data;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Collections;
+
 namespace CalorieCounter
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +20,110 @@ namespace CalorieCounter
         public Page2()
         {
             InitializeComponent();
+            //SearchingFoods.Text = "Searching Foods";
         }
+
+
+        private void Entry_Completed(object sender, TextChangedEventArgs e)
+        {
+
+            textCompleted.Text = ((Entry)sender).Text;
+
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextOldValue.Text = e.OldTextValue;
+            String connectionString;
+            String output1 = "";
+            String output2 = "";
+
+            connectionString = "Data Source=cec-stahrm-ws12.it.muohio.edu; Initial Catalog=CalorieCounter;User ID=CalorieCounter_Admin_99$;Password=CC_ADm1n_$$";
+
+
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                Console.WriteLine("Connection Open  !");
+
+
+
+                TextNewValue.Text = "Connection Open!";
+                SqlCommand command = new SqlCommand("Select * from Food where foodname like '%" + e.OldTextValue + "%' order by FoodName", sqlConnection);
+
+                SqlDataReader reader = null;
+                reader = command.ExecuteReader();
+                DataTable schemaTable = reader.GetSchemaTable();
+
+
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    foreach (DataColumn column in schemaTable.Columns)
+                    {
+                        //  output1+=String.Format("{0} = {1}",
+                        //     column.ColumnName, row[column])+"\n";
+
+                    }
+                }
+
+
+
+
+                while (reader.Read())
+                {
+                    String locName;
+                    int location = reader.GetInt32(1);
+                    switch (location)
+                    {
+                        case 1:
+                            locName = ("UDF");
+                            break;
+                        case 2:
+                            locName = ("Bell Tower");
+                            break;
+                        case 3:
+                            locName = ("Null");
+                            break;
+                        case 4:
+                            locName = ("Garden Commons");
+                            break;
+                        case 5:
+                            locName = ("King Cafe");
+                            break;
+                        case 6:
+                            locName = ("Market Street Cafe");
+                            break;
+                        case 7:
+                            locName = ("Martin Dining Hall");
+                            break;
+                        case 8:
+                            locName = ("ScoreBoard Market");
+                            break;
+                        case 9:
+                            locName = ("ScoreBoard Market");
+                            break;
+                        case 10:
+                            locName = ("MapleStreet Commons");
+                            break;
+
+                        default:
+                            locName = ("Unknown Location");
+                            break;
+
+                    }//end switch
+
+                    output2 += reader.GetString(12) + "  |\t" + locName + "\n";
+
+
+                }
+
+                //(String.Format("ID{0}  Loc{1} Type{2} Cal{3} Trans{4} Sat{5} chol{6} Sodium{",
+                //  column.ColumnName, row[column]));
+
+                TextOldValue.Text = output2;
+                sqlConnection.Close();
+            }//end using
+        }//end emthod entrychanged
     }
 }
