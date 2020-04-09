c5a99bad6c09a020e5ed9c9a5dfe7ac3a433b15d
Alec Bird <abird1231@gmail.com>
2020-04-09 16:54:38 -0400

    Updated authentication method and added image to welcome page

--- CalorieCounter/CalorieCounter/WelcomePage.xaml.cs
+++ CalorieCounter/CalorieCounter/WelcomePage.xaml.cs
@@ -21,6 +21,8 @@ namespace CalorieCounter
     public partial class WelcomePage : ContentPage
     {
         RestService _restService;
+        private string client_id;
+        private string redirect_uri;
         public WelcomePage()
         {
             InitializeComponent();
@@ -44,28 +46,7 @@ namespace CalorieCounter
             };
             NavigationPage.SetTitleView(this, header);
             _restService = new RestService();
-        }
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
-        {
-            Login();
-        }
-
-        private void SignUpButton_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new SignUpPage());
-        }
-
-        private void AboutButton_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
-        public async void Login()
-        {
-            // authenticate
-            string client_id = "";
-            string redirect_uri = "";
             if (Device.RuntimePlatform == Device.Android)
             {
                 client_id = "1041253101002-dhan7880g5t577r7d6lc8cfcsvqfqqhf.apps.googleusercontent.com";
@@ -78,7 +59,27 @@ namespace CalorieCounter
                 redirect_uri = "caloriecounter.oauth2:/oauth2redirect";
 
             }
-           
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            IdToken idToken = await AuthenticateUser();
+            await Navigation.PushModalAsync(new MainPage(idToken));
+
+        }
+
+        private void SignUpButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SignUpPage());
+        }
+
+        private void AboutButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        public async Task<IdToken> AuthenticateUser()
+        {
             string requestUri = "https://accounts.google.com/o/oauth2/v2/auth?";
             requestUri += $"scope=openid%20email%20profile";
             requestUri += $"&response_type=code";
@@ -90,7 +91,11 @@ namespace CalorieCounter
                 new Uri(requestUri),
                 new Uri(redirect_uri));
 
+            // while token is fetched to gather user info, disable page and show loading symbol 
             activityIndicator.IsRunning = true;
+            LoggingIn.IsVisible = true;
+            this.Content.IsEnabled = false;
+
             string code = authResult.Properties["code"];
             string content = $"code={code}&client_id={client_id}&redirect_uri={redirect_uri}&grant_type=authorization_code";
             string tokenUrl = "https://oauth2.googleapis.com/token";
@@ -108,7 +113,7 @@ namespace CalorieCounter
 
                 string j = decoder.Decode(result.IdToken);
                 idToken = JsonConvert.DeserializeObject<IdToken>(j);
-                
+
             }
             catch (TokenExpiredException)
             {
@@ -119,9 +124,11 @@ namespace CalorieCounter
                 Console.WriteLine("Token has invalid signature");
             }
 
-            // if user exists (if idToken matches idToken of user in DB) then login
             activityIndicator.IsRunning = false;
-            await Navigation.PushModalAsync(new MainPage(idToken));
+            LoggingIn.IsVisible = false;
+
+            // if user exists (if idToken matches idToken of user in DB) then login
+            return idToken;
 
         }
 
