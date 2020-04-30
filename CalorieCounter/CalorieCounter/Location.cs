9e87fd801e3a0e90226200581e76fbe532df4a7b
Alec Bird <abird1231@gmail.com>
2020-04-30 14:32:18 -0400

    Added a rest method to get active locations (locations that are in offered table) and added (not open) to locations list on page 2 if that location is not active

---
+++ CalorieCounter/CalorieCounter/Location.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalorieCounter
+{
+    [JsonObject]
+    public class Location
+    {
+        [JsonProperty("serviceUnit")]
+        public string ServiceUnit { get; set; }
+
+        [JsonProperty("itemCount")]
+        public string ItemCount { get; set; }
+    }
+}
